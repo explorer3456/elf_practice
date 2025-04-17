78ce3d753d31f9317b2a9f3e3508f5d25fdfb767
Ditto Kim <rlawhdgus34@gmail.com>
2025-04-18 01:47:39 +0900

    Using data section and add byte datas
    - note that before linking stage, data section and text section both starts
    at address 0x0. because both sections are not relocated yet.

---
+++ bare_metal_program/linker_script/add-mem.s
@@ -0,0 +1,22 @@
+.data
+val1:
+    .4byte 10
+val2:
+    .4byte 30
+result:
+    .4byte 0
+
+.text
+start:
+        ldr   r0, =val1         @ r0 = &val1
+        ldr   r1, =val2         @ r1 = &val2
+
+        ldr   r2, [r0]          @ r2 = *r0
+        ldr   r3, [r1]          @ r3 = *r1
+
+        add   r4, r2, r3        @ r4 = r2 + r3
+
+        ldr   r0, =result       @ r0 = &result
+        str   r4, [r0]          @ *r0 = r4
+
+stop:   b stop
