b5b88408421eef97b1d80bfcb4d6fad79db132a8
Ditto Kim <rlawhdgus34@gmail.com>
2025-04-19 22:50:59 +0900

    Data segment test with large bss data with init
    - when intialize array, it no longer become bss data, it is data section data.
    - so you can see that data segment file size and mem size become simllar

--- bare_metal_program/linker_script/c_prog.c
+++ bare_metal_program/linker_script/c_prog.c
@@ -1,7 +1,7 @@
 static int arr[] = { 1, 10, 4, 5, 6, 7 };
 static int sum;
 static const int n = sizeof(arr) / sizeof(arr[0]);
-static int array[2048];
+static int array[2048] = {1,2,3};
 
 int main()
 {
